b132319597199718444237e6cd5369e65af3d308
Aleksei Falchuk <sddw28@gmail.com>
2024-06-25 16:18:47 +0200

    feat: add static and config files

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       shared: "/src/shared",
       pages: "/src/pages",
       assets: "/src/assets",
       types: "/src/types",
-    }
-  }
-})
+      services: "/src/services",
+    },
+  },
+});
