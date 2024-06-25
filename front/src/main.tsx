fccc1b50a7af3a53941ec850a5d20f6a6f819043
Aleksei Falchuk <sddw28@gmail.com>
2024-06-25 16:30:15 +0200

    feat: add react-query and homepage

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {RouterProvider, createBrowserRouter} from "react-router-dom"
-import "../src/styles/main.scss"
-import "normalize.css"
-import { Root } from './shared/Root/Root.tsx'
-import { ErrorPage } from './pages/ErrorPage/ErrorPage.tsx'
-import { HomePage } from './pages/HomePage/HomePage.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Root } from "./shared/Root/Root.tsx";
+import { ErrorPage } from "./pages/ErrorPage/ErrorPage.tsx";
+import { HomePage } from "./pages/HomePage/HomePage.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "../src/styles/main.scss";
+import "normalize.css";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: "",
-                element: <HomePage />
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
---
+++ front/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header/Header";
+import { PersonCardsBlock } from "./components/PersonCardsBlock/PersonCardsBlock";
+
+
+export const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <PersonCardsBlock />
+    </>
+  );
+};
